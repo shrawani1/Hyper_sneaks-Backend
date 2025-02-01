3edbd8bcbd6785de7a95c60616223d420a82a3c9
Shrawani <Shrawanizzzkc@gmail.com>
2025-02-01 13:18:42 +0545

    review model

--- models/reviewModal.js
+++ models/reviewModal.js
@@ -1,36 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const reviewSchema = new mongoose.Schema({
-//     user: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'users',
-//         required: true
-//     },
-//     product: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'products',
-//         required: true
-//     },
-//     rating: {
-//         type: Number,
-//         required: true,
-//         min: 1,
-//         max: 5
-//     },
-//     comment: {
-//         type: String,
-//         required: true,
-//         maxLength: 500
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: Date.now
-//     }
-// });
-
-// const Review = mongoose.model('reviews', reviewSchema);
-
-// module.exports = Review;
 const mongoose = require("mongoose");
  
 const reviewSchema = new mongoose.Schema({
